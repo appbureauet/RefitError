3301c302080e599d47e762d4743cb1f39b6e63ec
Anders Bech Mellson <abm@appbureauet.dk>
2017-08-01 20:18:19 +0200

    init

---
+++ RefitError/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Xamarin.Forms;
+using RefitError.Services;
+using Refit;
+
+namespace RefitError
+{
+    public partial class App : Application
+    {
+        public static bool UseMockDataStore = true;
+        public static string BackendUrl = "https://localhost:5000";
+
+        public App()
+        {
+            InitializeComponent();
+
+            if (UseMockDataStore)
+                DependencyService.Register<MockDataStore>();
+            else
+                DependencyService.Register<CloudDataStore>();
+
+            var nonsenseApi = RestService.For<INonsenseApi>("https://api.nonsense.com");
+
+            var octocat = nonsenseApi.Nonsense("octocat");
+
+            if (Device.RuntimePlatform == Device.iOS)
+                MainPage = new MainPage();
+            else
+                MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
---
+++ RefitError/Services/IGitHubApi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Refit;
+
+namespace RefitError.Services
+{
+    public interface INonsenseApi
+    {
+        [Get("/superapi/{meaning}")]
+        Task<bool> Nonsense(string nosense);
+    }
+}
